df07831f5da849a8b18696bee9fe92b3552e0f0f
codebyalexx <a.legal.contact@gmail.com>
2025-04-14 23:39:49 +0200

    :zap: Improved responsivenes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { getMeals } from "@/actions/meals";
 import { auth, signIn } from "@/auth";
 import { DailyDietTracker } from "@/components/diet-tracker";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export default async function Home() {
   const session = await auth();
@@ -11,8 +12,11 @@ export default async function Home() {
   console.log(meals);
 
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <div className="w-full max-w-2xl px-2">
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-2xl p-4 py-4 space-y-6">
+        <h1 className="font-bold text-2xl flex items-center justify-center gap-2 w-full">
+          Diet Tracker <ThemeToggle />
+        </h1>
         <DailyDietTracker initialMeals={meals} />
       </div>
     </div>
